324a5a66e73b158a83f8bed71960d6284807cca2
Vanilson <vanrock02@hotmail.com>
2020-09-01 01:14:20 -0300

    segundo teste

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>funcionando dentro do esperado!</Text>
+      <Text>E se estiver funcionando dentro do esperado!</Text>
       <StatusBar style="auto" />
     </View>
   );
